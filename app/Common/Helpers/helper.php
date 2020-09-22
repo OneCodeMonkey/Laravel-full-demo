d7391592ec1150258347dea641751eb2a4996b0e
OneCodeMonkey <1460018362@qq.com>
2020-09-22 13:02:33 +0800

    fix style

--- app/Common/Helpers/helper.php
+++ app/Common/Helpers/helper.php
@@ -8,7 +8,8 @@ if (!function_exists('uuid')) {
      *
      * @return string
      */
-    function uuid() {
+    function uuid()
+    {
         return md5(md5(floor((explode(' ', microtime())[0] + explode(' ', microtime())[1]) * 1000)) . uniqid());
     }
 }
