230dafad1d3ab44984c55c78d246ad02bb7a562b
OneCodeMonkey <1460018362@qq.com>
2020-12-11 17:04:48 +0800

    feat

--- app/Common/Helpers/helper.php
+++ app/Common/Helpers/helper.php
@@ -13,3 +13,30 @@ if (!function_exists('uuid')) {
         return md5(md5(floor((explode(' ', microtime())[0] + explode(' ', microtime())[1]) * 1000)) . uniqid());
     }
 }
+
+if (!function_exists('formatTimestamp')) {
+    function formatTimestamp($timestamp)
+    {
+        $nowTime = time();
+        $showTime = is_numeric($timestamp) ? $timestamp : strtotime($timestamp);
+        $dur = $nowTime - $showTime;
+        if ($dur < 180) {
+            return '刚刚';
+        } else {
+            if ($dur < 3600) {
+                return floor($dur / 60) . '分钟前';
+            } else {
+                if ($dur < 86400) {
+                    return floor($dur / 3600) . '小时前';
+                } else {
+                    return date("n月j日 H:i", $showTime);
+//                    if ($dur < 864000) {
+//                        return floor($dur / 86400) . '天前';
+//                    } else {
+//                        return date("Y-m-d", $showTime);
+//                    }
+                }
+            }
+        }
+    }
+}
